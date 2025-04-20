7ff08099daa9a895dd9af6b18164604c2eb5b7ab
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2025-04-20 17:39:08 +0200

    Implements Failed Outperformance Ratio

--- leechdetector/LeechDetector.py
+++ leechdetector/LeechDetector.py
@@ -10,14 +10,23 @@ from AnkiValueParser import is_actual_review, is_failed, is_success, interval_to
 
 class LapseInfos:
 
-    def __init__(self, card_id : CardId, past_max_intervals : list[int], current_lapse_max_intervals : int):
+    def __init__(self, card_id : CardId, past_max_intervals : list[int], current_lapse_max_intervals : int, outperformance_factor : int = 1.25):
         self.card_id = card_id
         self.past_max_intervals = past_max_intervals
         self.current_lapse_max_intervals = current_lapse_max_intervals
         self.lapses_count = len(past_max_intervals)
+        self.outperformance_factor = outperformance_factor
 
     def drop_count(self):
         return sum([1 for i in range(1, self.lapses_count) if self.past_max_intervals[i - 1] > self.past_max_intervals[i]])
+
+    def failed_outperformance_count(self):
+        return sum([1 for i in range(1, self.lapses_count) if not self.past_max_intervals[i - 1] * self.outperformance_factor < self.past_max_intervals[i]])
+
+    def failed_outperformance_ratio(self):
+        if self.lapses_count <= 1:
+            return 0
+        return self.failed_outperformance_count() / (self.lapses_count - 1)
 
     def biggest_drop_value(self):
         if self.drop_count() == 0:
@@ -33,7 +42,7 @@ class LapseInfos:
 
     def __repr__(self):
         if self.lapses_count > 0:
-            return f'Card : {self.card_id} Past Lapses : ({self.lapses_count}) {self.past_max_intervals} (now:{self.current_lapse_max_intervals})  Drops:{self.drop_count()} BiggestDrop:{self.biggest_drop_value()} Mean:{self.average_max_interval():.2f} Median:{self.median_max_interval()}'
+            return f'Card : {self.card_id} Past Lapses : ({self.lapses_count}) {self.past_max_intervals} (now:{self.current_lapse_max_intervals})  Drops:{self.drop_count()} BiggestDrop:{self.biggest_drop_value()} Mean:{self.average_max_interval():.2f} Median:{self.median_max_interval()} FailedOutperforamnce:{self.failed_outperformance_count()} FailedOutperforamnceRatio:{self.failed_outperformance_ratio()*100:.2f}'
         else:
             return f'Card : {self.card_id} No Lapse, Current Max Interval : {self.current_lapse_max_intervals}'
 
--- leechdetector/test.py
+++ leechdetector/test.py
@@ -33,18 +33,35 @@ class LeechDetectorTest(unittest.TestCase):
         lapse_infos = []
         for card_id in self.collection.find_cards('"deck:Japan::1. Vocabulary" -is:new prop:lapses>0'):
             lapse_infos.append(self.leechdetector.get_max_successful_interval_by_lapse(card_id))
-        lapse_infos.sort(key=lambda lapseInfo: lapseInfo.lapses_count, reverse=True)
-        distribution_drops = group_lapse_info_by_key(lapse_infos, lambda lapseInfo: lapseInfo.drop_count())
-        distribution_lapse_count = group_lapse_info_by_key(lapse_infos, lambda lapseInfo: lapseInfo.lapses_count)
         total_card_count = len(self.collection.find_cards('"deck:Japan::1. Vocabulary" -is:new'))
+        lapsed_card_count = len(lapse_infos)
+        distribution_lapse_count = group_lapse_info_by_key(lapse_infos, lambda lapseInfo: lapseInfo.lapses_count)
+        distribution_drops = group_lapse_info_by_key(lapse_infos, lambda lapseInfo: lapseInfo.drop_count())
         print(f"Total Cards reviewed : {total_card_count}")
-        print(f"Cards That lapsed at least once: {len(lapse_infos)} ({len(lapse_infos) / total_card_count * 100:.2f}%)")
+        print(f"Cards That lapsed at least once: {lapsed_card_count} ({lapsed_card_count / total_card_count * 100:.2f}%)")
         print(f"Cards That had 0 dropping lapse: {distribution_drops[0]} ({distribution_drops[0] / total_card_count * 100:.2f}%)")
         print(f"Cards That had 1 dropping lapse: {distribution_drops[1]} ({distribution_drops[1] / total_card_count * 100:.2f}%)")
         card_count_other = sum([dropCount for (drop, dropCount) in distribution_drops.items() if drop > 1])
         print(f"Cards That had >1 dropping lapse : {card_count_other} ({card_count_other / total_card_count * 100:.2f}%)")
+
+        ## Failed Outperforamce Counts
+        failed_outperformance_counts = group_lapse_info_by_key(lapse_infos, lambda lapseInfo: lapseInfo.failed_outperformance_count())
+        print(f"Cards That had 0 failed outperformance lapse: {failed_outperformance_counts[0]} ({failed_outperformance_counts[0] / lapsed_card_count * 100:.2f}%)")
+        print(f"Cards That had 1 failed outperformance lapse: {failed_outperformance_counts[1]} ({failed_outperformance_counts[1] / lapsed_card_count * 100:.2f}%)")
+        card_count_other = sum([failed_outperformance_count_total for (failed_outperformance_count, failed_outperformance_count_total) in failed_outperformance_counts.items() if failed_outperformance_count > 1])
+        print(f"Cards That had >1 failed outperformance lapse : {card_count_other} ({card_count_other / lapsed_card_count * 100:.2f}%)")
+
+        ## Failed Outperformance Counts Ratio
+        failed_outperformance_ratio = group_lapse_info_by_key(lapse_infos, lambda lapseInfo: int(lapseInfo.failed_outperformance_ratio() * 100 / 33.34))
+        print(f"Cards That had [2/3, 1.0] failed outperformance ratio lapse: {failed_outperformance_ratio[2]} ({failed_outperformance_ratio[0] / lapsed_card_count * 100:.2f}%)")
+        print(f"Cards That had [1/3, 2.3] failed outperformance ratio lapse: {failed_outperformance_ratio[1]} ({failed_outperformance_ratio[1] / lapsed_card_count * 100:.2f}%)")
+        card_count_other = sum([failed_outperformance_ratio_total for (failed_outperformance_ratio, failed_outperformance_ratio_total) in failed_outperformance_ratio.items() if failed_outperformance_ratio > 1])
+        print(f"Cards That had [0.0, 1/3] failed outperformance ratio lapse : {card_count_other} ({card_count_other / lapsed_card_count * 100:.2f}%)")
+
+
         print(distribution_drops)
         print(distribution_lapse_count)
+        lapse_infos.sort(key=lambda lapseInfo: lapseInfo.failed_outperformance_ratio(), reverse=True)
         [ print(lapse_info) for lapse_info in lapse_infos ]
 
 def group_lapse_info_by_key(lapseInfos, key_function):
