cee97729f541a5f5cb56bfeb3606f8ffa6f88e9f
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2025-04-29 21:03:12 +0200

    Fix where assertions are made for compatibility with other addon

--- leechdetector/hooks.py
+++ leechdetector/hooks.py
@@ -40,16 +40,16 @@ def handle_browser_will_search(context: aqt.browser.SearchContext):
     Handles custom 'leeches:' search terms, parsing arguments
     and filtering cards based on LeechDetector results.
     """
-    check_cross_addon_compatibility(context)
 
     leech_filters = parse_search_for_leech_filters(context.search)
 
     context.search = re.sub(LEECH_SEARCH_RE, "*", context.search)
 
     if context.ids is None:
-        context.ids = aqt.mw.col.find_cards(context.search, context.order, context.reverse)
-
-    context.ids = filter_cards(context.ids, leech_filters)
+        if len(leech_filters) > 0:
+            check_cross_addon_compatibility(context)
+            context.ids = aqt.mw.col.find_cards(context.search, context.order, context.reverse)
+            context.ids = filter_cards(context.ids, leech_filters)
 
     return context
 
@@ -62,6 +62,7 @@ def parse_search_for_leech_filters(search):
 
 
 def check_cross_addon_compatibility(context):
+    print(f"context.order {context.order}")
     if context.order.key.startswith("_field"):  ## To ensure no crash with AdvancedBrowser addon
         context.order.key = "noteFld"
         showWarning(
@@ -72,8 +73,8 @@ def filter_cards(card_ids, leech_filters):
     leechdetector = LeechDetector()
     filtered_ids = []
     if len(leech_filters) > 0:
-        for leech_type, leech_args in leech_filters.items():
-            for card_id in card_ids:
+        for card_id in card_ids:
+            for leech_type, leech_args in leech_filters.items():
                 lapse_infos = leechdetector.get_lapse_infos(card_id)
                 lapse_infos.configure_leech_detection(**leech_args)
                 leech_type_to_predicate = {
